80a1b12c497caf47af693282ee656143242d8107
acc <qwu@xiaohongshu.com>
2017-03-03 15:11:01 +0800

    set mybatis

---
+++ src/main/java/accew/comment/dao/CommentDao.java
@@ -0,0 +1,12 @@
+package accew.comment.dao;
+
+import accew.comment.model.Comment;
+import accew.comment.modules.mybatis.BaseMyBatisDao;
+import accew.comment.modules.mybatis.MyBatisDao;
+
+/**
+ * Created by acc on 2017/3/3.
+ */
+@MyBatisDao
+public interface CommentDao extends BaseMyBatisDao<Comment, Long>{
+}
---
+++ src/main/java/accew/comment/modules/mybatis/BaseMyBatisDao.java
@@ -0,0 +1,25 @@
+package accew.comment.modules.mybatis;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+public interface BaseMyBatisDao<T, PK extends Serializable> {
+
+	T selectByPrimaryKey(PK id);
+
+	void deleteByPrimaryKey(PK id);
+
+	void insert(T entity);
+
+	void insertSelective(T entity);
+
+	void updateByPrimaryKey(T entity);
+
+	void updateByPrimaryKeySelective(T entity);
+
+	List<T> selectList(T entity);
+
+	List<T> selectList(Map<String, Object> paramMap);
+
+}
